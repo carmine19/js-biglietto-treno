b5baeebbf44fe190a10eb96b2f7c84f9f39cc3f1
carmine <carminepepiciello@gmail.com>
2020-10-13 17:52:19 +0200

    fix nomi

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@
 
 // imposto il prezzo del biglietto, questo è di 0.21 euro al km
 
-// imposto la distanza
+// imposto i km da percorrere
 
 // sconto il prezzo per minorenni, quindi persone che hanno meno di  18 anni, qui il prezzo è del 20% in meno
 
-//sconto il prezzo per anziani, persone che hanno oltre 65 anni, qui il prezzo è del 20% in più
+//sconto il prezzo per anziani, persone che hanno oltre 65 anni, qui il prezzo è del 40% in meno
 
-// calcolo quindi il prezzo del biglietto - il 20 o 40 %
+// calcolo quindi i prezzi dei biglietti
 
 
 let prezzoBiglietto = 0.21;
@@ -20,7 +20,6 @@ scontoOver65PerKm = (prezzoBiglietto - scontoOver65PerKm).toFixed(2);
 console.log(scontoOver65PerKm);
 
 let scontoUnder18PerKm = (prezzoBiglietto * 20) / 100;
-console.log(scontoUnder18PerKm)
 scontoUnder18PerKm = (prezzoBiglietto - scontoUnder18PerKm).toFixed(2);
 console.log(scontoUnder18PerKm);
 
@@ -33,19 +32,19 @@ if(isNaN(etaUtente) == false && (etaUtente > 5 && etaUtente < 120) ){
     alert(`devi inserire un numero corretto`)
 }
 
-let destinazione = parseInt(prompt('Quanti km devi percorrere?'))
-console.log(destinazione)
+let kmDaPercorrere = parseInt(prompt('Quanti km devi percorrere?'))
+console.log(kmDaPercorrere)
 
-if(isNaN(destinazione) == false){
+if(isNaN(kmDaPercorrere) == false){
     console.log('numero corretto')
 }else {
     alert(`devi inserire un numero`)
 }
 
 if( etaUtente > 65) {
-    document.getElementById('password').innerHTML='il tuo prezzo è di'+ '&nbsp' + scontoOver65PerKm * destinazione + '' + '€ scontato del 40%';
+    document.getElementById('password').innerHTML='il tuo prezzo è di'+ '&nbsp' + scontoOver65PerKm * kmDaPercorrere + '' + '€ scontato del 40%';
 } else if ( etaUtente < 18) {
-    document.getElementById('password').innerHTML='il tuo prezzo è di' + '&nbsp' + scontoUnder18PerKm * destinazione + '' + '€ scontato del 20%';
+    document.getElementById('password').innerHTML='il tuo prezzo è di' + '&nbsp' + scontoUnder18PerKm * kmDaPercorrere + '' + '€ scontato del 20%';
 } else {
-    document.getElementById('password').innerHTML='il tuo prezzo è di' + '&nbsp' + prezzoBiglietto * destinazione + '&nbsp' + '€';
+    document.getElementById('password').innerHTML='il tuo prezzo è di' + '&nbsp' + prezzoBiglietto * kmDaPercorrere + '&nbsp' + '€';
 }
